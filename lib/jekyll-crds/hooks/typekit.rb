85c709b5af7c826e93a178d3c480b41014024226
Taylor C. MacDonald <tcmacdonald@gmail.com>
2019-05-28 07:58:23 -0400

    US17295 Jekyll Updates Make path to scss file configurable

--- lib/jekyll-crds/hooks/typekit.rb
+++ lib/jekyll-crds/hooks/typekit.rb
@@ -6,7 +6,7 @@ Jekyll::Hooks.register(:site, :after_init) do |site|
     raise "Missing required config option: typekit_url" if site.config['typekit_url'].nil?
     # Download CSS from TypeKit.
     fonts_css = open(site.config['typekit_url']) { |f| f.read }
-    css_file = '_assets/stylesheets/public/_fonts.scss'
+    css_file = File.join(Dir.pwd, site.config['typekit_scss'] || '_assets/stylesheets/public/_fonts.scss')
     # If CSS is different from what we currently have, write to the fonts partial.
     File.open(css_file, 'w+') { |f| f.write(fonts_css) } unless fonts_css == File.read(css_file)
   end
