21e5fdd3e099ef6051d965dd81d2efc4b3012b56
Sean C Davis <scdavis41@gmail.com>
2019-08-12 09:43:47 -0400

    US17681 - Remove Songs/Albums Add catch for albums on media_url_filter

--- lib/jekyll-crds/filters/media_url_filter.rb
+++ lib/jekyll-crds/filters/media_url_filter.rb
@@ -2,11 +2,13 @@ module Jekyll
   module MediaUrlFilter
 
     def media_url(item, arg = nil)
-      return nil if item.nil? 
+      return nil if item.nil?
       if item['content_type'] == 'song' && item['album']
         "#{ENV['CRDS_MUSIC_ENDPOINT'].chomp('/')}/music/#{item['album']['slug']}/#{item['slug']}"
+      elsif item['content_type'] == 'album'
+        "#{ENV['CRDS_MUSIC_ENDPOINT'].chomp('/')}/music/#{item['slug']}"
       else
-        item["bitmovin_url"] || (item["related_videos"] && item["related_videos"][0]["bitmovin_url"]) ? append_video_params(item, arg) : item.url 
+        item["bitmovin_url"] || (item["related_videos"] && item["related_videos"][0]["bitmovin_url"]) ? append_video_params(item, arg) : item.url
       end
     end
 
@@ -17,8 +19,8 @@ module Jekyll
         "#{item.url}?autoPlay=true&sound=11"
       end
     end
+
   end
-  
 end
 
 Liquid::Template.register_filter(Jekyll::MediaUrlFilter)
