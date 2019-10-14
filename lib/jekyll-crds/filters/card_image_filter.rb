64a10d5dd91f96903c934240deb9993b4cb25856
Taylor C. MacDonald <tcmacdonald@gmail.com>
2019-10-14 16:37:49 -0400

    DE7188 Card Images Refactor for podcast

--- lib/jekyll-crds/filters/card_image_filter.rb
+++ lib/jekyll-crds/filters/card_image_filter.rb
@@ -3,8 +3,10 @@ module Jekyll
     def card_image(doc)
       item = doc.instance_variable_get('@obj').try('data')
       case item.dig('content_type')
-      when 'song', 'album', 'message', 'episode'
+      when 'song', 'album', 'message'
         item.dig('background_image', 'url') || item.dig('bg_image', 'url') || item.dig('image', 'url')
+      when 'episode'
+        item.dig('podcast', 'bg_image', 'url') || item.dig('podcast', 'image', 'url')
       else
         item.dig('image', 'url')
       end
