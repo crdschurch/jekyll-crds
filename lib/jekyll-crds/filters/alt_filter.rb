a7bfd09e283eae467fff778f3477a7adef1dd20b
Taylor C. MacDonald <taylor@helloample.com>
2020-12-16 16:17:00 -0500

    Ensure file exists

--- lib/jekyll-crds/filters/alt_filter.rb
+++ lib/jekyll-crds/filters/alt_filter.rb
@@ -4,12 +4,13 @@ module Jekyll
       if input.present?
         id = input.is_a?(String) ? input : input.dig('id')
         path = File.join(site.collections_path, "_assets", "#{id}.md")
-        obj = YAML.load(File.read(path))
-        (obj.dig('description') || obj.dig('title') || nil) unless obj.nil? || obj.is_a?(String)
+        if File.exists?(path)
+          obj = YAML.load(File.read(path))
+          (obj.dig('description') || obj.dig('title') || nil) unless obj.nil?
+        end
       end
     end
   end
-
 end
 
 Liquid::Template.register_filter(Jekyll::AltFilter)
